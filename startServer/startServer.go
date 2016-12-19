2af6c4539d280fb2e9d0ae8d1506dd402e812f7f
Armienn <nigathr@gmail.com>
2016-12-19 20:29:25 +0100

    Played a bit with templates

--- startServer/startServer.go
+++ startServer/startServer.go
@@ -1,26 +1,31 @@
 package main
 
 import (
+	"html/template"
 	"net/http"
-	"strconv"
 
 	"github.com/Armienn/GoServer"
 )
 
 func main() {
-	server := goserver.NewServer(true)
+	server := goserver.NewServer(false)
 	server.AddHandler("/", viewHandler)
-	server.AddUser("kristjan", "cool")
 	server.Serve()
 }
 
 func viewHandler(server *goserver.Server, w http.ResponseWriter, r *http.Request, path string, session goserver.Session, user interface{}) {
-	count := 0
+	data := struct{ Count int }{0}
 	value, ok := session.Get("musle")
 	if ok {
-		count = value.(int)
+		data.Count = value.(int)
 	}
-	count++
-	session.Set("musle", count)
-	w.Write([]byte("Jo hollo" + strconv.Itoa(count)))
+	data.Count++
+	session.Set("musle", data.Count)
+	temp, err := template.ParseFiles("test.html")
+	if err != nil {
+		w.Write([]byte("Fejl: " + err.Error()))
+	} else {
+		temp.Execute(w, data)
+	}
+	//w.Write([]byte("Jo hollo" + strconv.Itoa(count)))
 }
