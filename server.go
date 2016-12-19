035e046ee0de6d162ac8619a61fafb5c28905434
Armienn <nigathr@gmail.com>
2016-12-19 13:22:55 +0100

    Added some structure

--- server.go
+++ server.go
@@ -3,10 +3,42 @@ package main
 import "net/http"
 
 func main() {
-	http.HandleFunc("/view/", viewHandler)
-	http.ListenAndServe(":8080", nil)
+	server := NewServer()
+	server.AddHandler("/view/", viewHandler)
+	server.Serve()
 }
 
-func viewHandler(w http.ResponseWriter, r *http.Request) {
+func viewHandler(w http.ResponseWriter, r *http.Request, path string) {
 	w.Write([]byte("Jo hollo"))
+}
+
+type Server struct {
+	Handlers map[string]func(http.ResponseWriter, *http.Request, string)
+}
+
+func NewServer() *Server {
+	server := new(Server)
+	server.Handlers = make(map[string]func(http.ResponseWriter, *http.Request, string))
+	return server
+}
+
+func (server *Server) AddHandler(path string, handler func(http.ResponseWriter, *http.Request, string)) {
+	server.Handlers[path] = handler
+}
+
+func (server *Server) ServeOnPort(port string) {
+	for path, handler := range server.Handlers {
+		http.HandleFunc(path, makeHandler(path, handler))
+	}
+	http.ListenAndServe(port, nil)
+}
+
+func (server *Server) Serve() {
+	server.ServeOnPort(":8080")
+}
+
+func makeHandler(path string, handler func(http.ResponseWriter, *http.Request, string)) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		handler(w, r, r.URL.Path[len(path):])
+	}
 }
