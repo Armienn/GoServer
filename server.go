21e85e3c1ee44abcb29e2c72ced0ab5b1e894e4f
Armienn <nigathr@gmail.com>
2016-12-19 17:17:54 +0100

    Started added login functionality

--- server.go
+++ server.go
@@ -5,18 +5,20 @@ import (
 )
 
 type Server struct {
-	Handlers       map[string]func(http.ResponseWriter, *http.Request, string, Session)
+	Handlers       map[string]func(http.ResponseWriter, *http.Request, string, Session, interface{})
 	SessionManager *SessionManager
+	RequireLogin   bool
 }
 
-func NewServer() *Server {
+func NewServer(requireLogin bool) *Server {
 	server := new(Server)
-	server.Handlers = make(map[string]func(http.ResponseWriter, *http.Request, string, Session))
+	server.Handlers = make(map[string]func(http.ResponseWriter, *http.Request, string, Session, interface{}))
 	server.SessionManager = NewSessionManager("sessionid", 3600)
+	server.RequireLogin = requireLogin
 	return server
 }
 
-func (server *Server) AddHandler(path string, handler func(http.ResponseWriter, *http.Request, string, Session)) {
+func (server *Server) AddHandler(path string, handler func(http.ResponseWriter, *http.Request, string, Session, interface{})) {
 	server.Handlers[path] = handler
 }
 
@@ -31,8 +33,42 @@ func (server *Server) Serve() {
 	server.ServeOnPort(":8080")
 }
 
-func (server *Server) makeHandler(path string, handler func(http.ResponseWriter, *http.Request, string, Session)) func(http.ResponseWriter, *http.Request) {
+func (server *Server) makeHandler(path string, handler func(http.ResponseWriter, *http.Request, string, Session, interface{})) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
-		handler(w, r, r.URL.Path[len(path):], server.SessionManager.SessionStart(w, r))
+		session := server.SessionManager.SessionStart(w, r)
+		user, ok := session.Get("user")
+		if !ok && server.RequireLogin {
+			if r.Method == "POST" {
+				handleLogin(w, r, session)
+			} else {
+				http.Redirect(w, r, "/login", http.StatusTemporaryRedirect)
+			}
+		} else {
+			handler(w, r, r.URL.Path[len(path):], server.SessionManager.SessionStart(w, r), user)
+		}
 	}
+}
+
+func handleLogin(w http.ResponseWriter, r *http.Request, session Session) {
+	if r.URL.Path != "/login" {
+		http.Redirect(w, r, "/login", http.StatusTemporaryRedirect)
+		return
+	}
+	err := r.ParseForm()
+	if err != nil {
+		http.Redirect(w, r, "/login", http.StatusTemporaryRedirect)
+		return
+	}
+	users, _ := r.Form["user"]
+	passwords, _ := r.Form["password"]
+	if len(users) > 0 && len(passwords) > 0 && userExists(users[0], passwords[0]) {
+		session.Set("user", users[0])
+		http.Redirect(w, r, "/", http.StatusAccepted)
+		return
+	}
+	http.Redirect(w, r, "/login", http.StatusTemporaryRedirect)
+}
+
+func userExists(user string, password string) bool {
+	return user == password
 }
--- startServer/startServer.go
+++ startServer/startServer.go
@@ -8,18 +8,18 @@ import (
 )
 
 func main() {
-	server := goserver.NewServer()
+	server := goserver.NewServer(true)
 	server.AddHandler("/view/", viewHandler)
 	server.Serve()
 }
 
-func viewHandler(w http.ResponseWriter, r *http.Request, path string, session goserver.Session) {
+func viewHandler(w http.ResponseWriter, r *http.Request, path string, session goserver.Session, user interface{}) {
 	count := 0
 	value, ok := session.Get("musle")
 	if ok {
 		count = value.(int)
 	}
-	count += 1
+	count++
 	session.Set("musle", count)
 	w.Write([]byte("Jo hollo" + strconv.Itoa(count)))
 }
