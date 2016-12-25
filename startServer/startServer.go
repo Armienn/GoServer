af3baf9116457edaf77867db5e0579322bb01b1b
Armienn <nigathr@gmail.com>
2016-12-25 16:54:44 +0100

    Removed gopherjs stuff

--- startServer/startServer.go
+++ startServer/startServer.go
@@ -4,36 +4,27 @@ import (
 	"html/template"
 	"net/http"
 
-	"io/ioutil"
-
 	"github.com/Armienn/GoServer"
 )
 
 func main() {
 	server := goserver.NewServer(false)
-	server.AddHandler("/js/", jsHandler)
 	server.AddHandler("/", viewHandler)
 	server.Serve()
 }
 
 func viewHandler(w http.ResponseWriter, r *http.Request, info goserver.Info) {
 	data := struct{ Count int }{0}
-	value, ok := session.Get("musle")
+	value, ok := info.Session.Get("musle")
 	if ok {
 		data.Count = value.(int)
 	}
 	data.Count++
-	session.Set("musle", data.Count)
+	info.Session.Set("musle", data.Count)
 	temp, err := template.ParseFiles("test.html")
 	if err != nil {
 		w.Write([]byte("Fejl: " + err.Error()))
 	} else {
 		temp.Execute(w, data)
 	}
-	//w.Write([]byte("Jo hollo" + strconv.Itoa(count)))
-}
-
-func jsHandler(w http.ResponseWriter, r *http.Request, info goserver.Info) {
-	file, _ := ioutil.ReadFile(path)
-	w.Write(file)
 }
